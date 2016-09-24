5ca1dc90f763dda96ec3896a8cdb41847f1999c1
Vamsi Devalla <S528117@mail.nwmissouri.edu>
2016-09-23 23:53:42 -0500

    Add files via upload

---
+++ Calculate.js
@@ -0,0 +1,6 @@
+function myfunction(){
+var weight= document.getElementById('weight').value;
+var height= document.getElementById('height').value;
+var bmi= (parseFloat(weight)*703)/(parseFloat(height)*parseFloat(height));
+  document.getElementById("bmi").innerHTML=bmi;
+};
---
+++ README.txt
@@ -0,0 +1,5 @@
+My First Assignment:
+	
+	This assignment is about creating a personal website with three html pages. In the later assignments we may include java script for the same assignment to enhance the website. So for now this is all we got!! Have fun!!
+
+-Devalla
